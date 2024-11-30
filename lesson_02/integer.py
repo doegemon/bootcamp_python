d32bf22c1f292e4e728e6f3a7559049da6f47c81
Nicolas Doege <github.repsu@passinbox.com>
2024-11-30 14:36:17 -0300

    adding integer examples from lesson 02

---
+++ lesson_02/integer.py
@@ -0,0 +1,32 @@
+# I. Ask the user to enter 2 integers and display the sum
+n1 = int(input('Enter the first number: '))
+n2 = int(input('Enter the second number: '))
+sum = n1 + n2
+
+print(f'The sum of the two number is: {sum}')
+
+# II. Ask the user to enter a number/integer and display the number divided by 5
+num = int(input('Enter a number: '))
+div = num / 5
+
+print(f'Your number divided by 5 is equal to: {div}')
+
+# III. Ask the user to enter 2 numbers and multiply them
+num_1 = int(input('Enter the first number: '))
+num_2 = int(input('Enter the second number: '))
+multi = num_1 * num_2
+
+print(f'{num_1} multiplied by {num_2} is equal to {multi}')
+
+# IV. Ask the user to enter 2 numbers and divide the first by the second
+n3 = int(input('Enter the first number: '))
+n4 = int(input('Enter the second number: '))
+division = n3 / n4
+
+print(f'{n3} divided by {n4} is equal to {division}')
+
+# V. Ask the user to enter a number and square this number
+n5 = int(input('Enter a number: '))
+square = n5 ** 2
+
+print(f'Your number squared is equal to: {square}')

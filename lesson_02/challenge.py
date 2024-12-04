65177edbed449b898d8cdffe686c85a82a3fcb22
Nicolas Doege <github.repsu@passinbox.com>
2024-12-04 14:21:57 -0300

    lesson 02: fix in challenge code

--- lesson_02/challenge.py
+++ lesson_02/challenge.py
@@ -7,10 +7,13 @@ try:
     raise ValueError("You have to type a name")
   elif any(char.isdigit() for char in name): 
     raise ValueError("The name can't have numbers in it.")
+  elif name.isspace():
+    raise ValueError("The name can't be empty.")
   else: 
     print("Valid name.")
 except ValueError as e:
   print(e)
+  exit()
 
 # II. Ask the user to type their salary and convert it to float
 try: 
@@ -19,10 +22,12 @@ try:
 
   if slry_float <= 0:
     print("Your salary can't be negative or equal to zero.")
+    exit()
   else:
     print("Valid salary.")
 except ValueError:
   print("Invalid salary, please use numbers only.")
+  exit()
 
 # III. Ask the user to type their bonus factor
 try:
@@ -30,10 +35,12 @@ try:
 
   if factor <= 0:
     print("Your bonus factor can't be negative or equal to zero.")
+    exit()
   else:
     print("Valid bonus factor.")
 except ValueError:
   print("Invalid bonus factor, please use numbers only.")
+  exit()
 
 # IV. Calculate the bonus amount (1000 + salary * bonus factor)
 FIXED_VALUE = 1000

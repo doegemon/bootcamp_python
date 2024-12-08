23673928c117425fa0cd3a3b96f8067bf547410f
Nicolas Doege <github.repsu@passinbox.com>
2024-12-08 16:37:47 -0300

    lesson 04: add csv reading file and example data

---
+++ lesson_04/read_file.py
@@ -0,0 +1,15 @@
+# Reading a .csv file without using Pandas (using native Python library/package csv)
+import csv
+
+path: str = "data/data.csv"
+data: list = []
+
+# This function is kind of a Python API
+with open(file=path, mode="r", encoding="utf-8") as file: 
+  csv_reader: csv.DictReader = csv.DictReader(file)
+
+  for row in csv_reader:
+    data.append(row)
+
+for r in data: 
+  print(r)
